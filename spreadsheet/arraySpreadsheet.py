9326a8ba419910eeea119249b378a5f5797084f1
Calvin <s3950689@student.rmit.edu.au>
2023-04-13 16:57:07 +1000

    arraysDone

--- spreadsheet/arraySpreadsheet.py
+++ spreadsheet/arraySpreadsheet.py
@@ -13,26 +13,34 @@ from spreadsheet.baseSpreadsheet import BaseSpreadsheet
 class ArraySpreadsheet(BaseSpreadsheet):
 
     def __init__(self):
+        # print("Please enter a list of tuples of form (row,column,value) seperate dby commas: ")
         # TO BE IMPLEMENTED
         pass
 
-
-    def buildSpreadsheet(self, lCells: [Cell]):
+    def buildSpreadsheet(self, lCells: list[Cell]):
         """
         Construct the data structure to store nodes.
         @param lCells: list of cells to be stored
         """
+        mRow = max(lCells, key=lambda c: c.row).row
+        mCol = max(lCells, key=lambda c: c.col).col
 
-        # TO BE IMPLEMENTED
-        pass
 
+        self.spreadsheet = [[None] * (mCol + 1) for _ in range(mRow + 1)]
+        for cell in lCells:
+            self.update(cell.row, cell.col, cell.val)
 
-    def appendRow(self)->bool:
+    # TO BE IMPLEMENTED
+    pass
+
+    def appendRow(self) -> bool:
         """
         Appends an empty row to the spreadsheet.
 
         @return True if operation was successful, or False if not.
         """
+        col = self.colNum()
+        self.spreadsheet.append([None] * col)
 
         # TO BE IMPLEMENTED
         pass
@@ -40,22 +48,22 @@ class ArraySpreadsheet(BaseSpreadsheet):
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return True
 
-
-    def appendCol(self)->bool:
+    def appendCol(self) -> bool:
         """
         Appends an empty column to the spreadsheet.
 
         @return True if operation was successful, or False if not.
         """
-
+        rows = self.rowNum()
+        for row in self.spreadsheet:
+            row.append(None)
         # TO BE IMPLEMENTED
         pass
 
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return True
 
-
-    def insertRow(self, rowIndex: int)->bool:
+    def insertRow(self, rowIndex: int) -> bool:
         """
         Inserts an empty row into the spreadsheet.
 
@@ -63,15 +71,16 @@ class ArraySpreadsheet(BaseSpreadsheet):
 
         @return True if operation was successful, or False if not, e.g., rowIndex is invalid.
         """
-
+        if rowIndex < -1 or rowIndex >= self.rowNum():
+            return False
+        self.spreadsheet.insert(rowIndex + 1, [None] * self.colNum())
         # TO BE IMPLEMENTED
         pass
 
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return True
 
-
-    def insertCol(self, colIndex: int)->bool:
+    def insertCol(self, colIndex: int) -> bool:
         """
         Inserts an empty column into the spreadsheet.
 
@@ -79,13 +88,15 @@ class ArraySpreadsheet(BaseSpreadsheet):
 
         return True if operation was successful, or False if not, e.g., colIndex is invalid.
         """
-
+        if colIndex < -1 or colIndex >= self.colNum():
+            return False
+        for row in self.spreadsheet:
+            row.insert(colIndex + 1, None)
         # TO BE IMPLEMENTED
         pass
 
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return True
-
 
     def update(self, rowIndex: int, colIndex: int, value: float) -> bool:
         """
@@ -97,62 +108,64 @@ class ArraySpreadsheet(BaseSpreadsheet):
 
         @return True if cell can be updated.  False if cannot, e.g., row or column indices do not exist.
         """
-
+        if rowIndex < 0 or rowIndex >= self.rowNum() or colIndex < 0 or colIndex >= self.colNum():
+            return False
+        self.spreadsheet[rowIndex][colIndex] = value
         # TO BE IMPLEMENTED
         pass
 
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return True
 
-
-    def rowNum(self)->int:
+    def rowNum(self) -> int:
         """
         @return Number of rows the spreadsheet has.
         """
-
-        # TO BE IMPLEMENTED
+        return len(self.spreadsheet)
         pass
 
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return 0
-
-
-    def colNum(self)->int:
+    def colNum(self) -> int:
         """
         @return Number of column the spreadsheet has.
         """
-
-        # TO BE IMPLEMENTED
+        if len(self.spreadsheet) == 0:
+            return 0
+        else:
+            return len(self.spreadsheet[0])
         pass
 
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return 0
-
-
-
-    def find(self, value: float) -> [(int, int)]:
+    def find(self, value: float) -> list[(int, int)]:
         """
         Find and return a list of cells that contain the value 'value'.
 
         @param value value to search for.
 
         @return List of cells (row, col) that contains the input value.
-	    """
-
+            """
+        cellVal = []
+        for rowIndex,row in enumerate(self.spreadsheet):
+            for colIndex,cell in enumerate(row):
+                if cell == value:
+                    cellVal.append((rowIndex,colIndex))
+                    
         # TO BE IMPLEMENTED
+        return cellVal
         pass
 
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return []
 
-
-
-    def entries(self) -> [Cell]:
+    def entries(self) -> list[Cell]:
         """
         @return A list of cells that have values (i.e., all non None cells).
         """
-
+        cellVal = []
+        for rowIndex,row in enumerate(self.spreadsheet):
+            for colIndex,cell in enumerate(row):
+                if cell != None:
+                    cellVal.append(Cell(rowIndex,colIndex,cell))
         # TO BE IMPLEMENTED
+        return cellVal
         pass
 
         # TO BE IMPLEMENTED
