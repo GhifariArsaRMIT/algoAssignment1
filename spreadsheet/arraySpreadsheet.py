cce5c5a5beae58faf9474435d6e0bd9eab5bacf7
Calvin <s3950689@student.rmit.edu.au>
2023-04-13 17:02:43 +1000

    ccc

--- spreadsheet/arraySpreadsheet.py
+++ spreadsheet/arraySpreadsheet.py
@@ -18,10 +18,6 @@ class ArraySpreadsheet(BaseSpreadsheet):
         pass
 
     def buildSpreadsheet(self, lCells: list[Cell]):
-        """
-        Construct the data structure to store nodes.
-        @param lCells: list of cells to be stored
-        """
         mRow = max(lCells, key=lambda c: c.row).row
         mCol = max(lCells, key=lambda c: c.col).col
 
@@ -34,11 +30,6 @@ class ArraySpreadsheet(BaseSpreadsheet):
     pass
 
     def appendRow(self) -> bool:
-        """
-        Appends an empty row to the spreadsheet.
-
-        @return True if operation was successful, or False if not.
-        """
         col = self.colNum()
         self.spreadsheet.append([None] * col)
 
@@ -49,11 +40,6 @@ class ArraySpreadsheet(BaseSpreadsheet):
         return True
 
     def appendCol(self) -> bool:
-        """
-        Appends an empty column to the spreadsheet.
-
-        @return True if operation was successful, or False if not.
-        """
         rows = self.rowNum()
         for row in self.spreadsheet:
             row.append(None)
@@ -64,13 +50,6 @@ class ArraySpreadsheet(BaseSpreadsheet):
         return True
 
     def insertRow(self, rowIndex: int) -> bool:
-        """
-        Inserts an empty row into the spreadsheet.
-
-        @param rowIndex Index of the existing row that will be after the newly inserted row.  If inserting as first row, specify rowIndex to be 0.  If inserting a row after the last one, specify rowIndex to be rowNum()-1.
-
-        @return True if operation was successful, or False if not, e.g., rowIndex is invalid.
-        """
         if rowIndex < -1 or rowIndex >= self.rowNum():
             return False
         self.spreadsheet.insert(rowIndex + 1, [None] * self.colNum())
@@ -81,13 +60,6 @@ class ArraySpreadsheet(BaseSpreadsheet):
         return True
 
     def insertCol(self, colIndex: int) -> bool:
-        """
-        Inserts an empty column into the spreadsheet.
-
-        @param colIndex Index of the existing column that will be after the newly inserted row.  If inserting as first column, specify colIndex to be 0.  If inserting a column after the last one, specify colIndex to be colNum()-1.
-
-        return True if operation was successful, or False if not, e.g., colIndex is invalid.
-        """
         if colIndex < -1 or colIndex >= self.colNum():
             return False
         for row in self.spreadsheet:
@@ -99,15 +71,6 @@ class ArraySpreadsheet(BaseSpreadsheet):
         return True
 
     def update(self, rowIndex: int, colIndex: int, value: float) -> bool:
-        """
-        Update the cell with the input/argument value.
-
-        @param rowIndex Index of row to update.
-        @param colIndex Index of column to update.
-        @param value Value to update.  Can assume they are floats.
-
-        @return True if cell can be updated.  False if cannot, e.g., row or column indices do not exist.
-        """
         if rowIndex < 0 or rowIndex >= self.rowNum() or colIndex < 0 or colIndex >= self.colNum():
             return False
         self.spreadsheet[rowIndex][colIndex] = value
@@ -118,16 +81,10 @@ class ArraySpreadsheet(BaseSpreadsheet):
         return True
 
     def rowNum(self) -> int:
-        """
-        @return Number of rows the spreadsheet has.
-        """
         return len(self.spreadsheet)
         pass
 
     def colNum(self) -> int:
-        """
-        @return Number of column the spreadsheet has.
-        """
         if len(self.spreadsheet) == 0:
             return 0
         else:
@@ -135,13 +92,6 @@ class ArraySpreadsheet(BaseSpreadsheet):
         pass
 
     def find(self, value: float) -> list[(int, int)]:
-        """
-        Find and return a list of cells that contain the value 'value'.
-
-        @param value value to search for.
-
-        @return List of cells (row, col) that contains the input value.
-            """
         cellVal = []
         for rowIndex,row in enumerate(self.spreadsheet):
             for colIndex,cell in enumerate(row):
@@ -156,9 +106,6 @@ class ArraySpreadsheet(BaseSpreadsheet):
         return []
 
     def entries(self) -> list[Cell]:
-        """
-        @return A list of cells that have values (i.e., all non None cells).
-        """
         cellVal = []
         for rowIndex,row in enumerate(self.spreadsheet):
             for colIndex,cell in enumerate(row):
