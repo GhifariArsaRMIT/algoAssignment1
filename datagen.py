033e87e910d8d44fcd37a90329e500c1764ba909
GhifariArsa <s3893099@student.rmit.edu.au>
2023-04-19 03:08:07 +1000

    datageneration done

--- datagen.py
+++ datagen.py
@@ -1,10 +1,32 @@
 import random
 
 # Generate a file with random numbers and their line numbers
-with open('randomData.txt', 'w') as file:
-    for i in range(0, 100):
-        for j in range(0, 100):
-            # Generate a random integer
-            random_integer = round(random.uniform(1.0, 1000.0), 2)
-            # Write the line number and random integer to the file
-            file.write(f'{i} {j} {random_integer}\n')
+
+def createFile(size: str):
+    filename = "randomData"
+    ranges = 0
+    if size == "Small":
+        ranges = 15
+    elif size == "Medium":
+        ranges = 40
+    elif size == "Large":
+        ranges = 100
+        
+    for i in range(2):
+        filename += size
+        filename += str(i + 1)
+        filename += ".txt"
+        with open(filename, 'w') as file:
+            for i in range(0, ranges * 2 if i == 1 else ranges):
+                for j in range(0, ranges * 2 if i == 1 else ranges):
+                    # Generate a random integer
+                    random_integer = round(random.uniform(1.0, 1000.0), 2)
+                    # Write the line number and random integer to the file
+                    file.write(f'{i} {j} {random_integer}\n')
+        filename = "randomData"
+
+createFile("Small")
+createFile("Medium")
+createFile("Large")
+                
+        
